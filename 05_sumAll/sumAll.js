6f6fe6009aad6abb1e14d1bd67424c9b8c35100b
nikoroytio <roytioniko@gmail.com>
2023-09-28 10:41:49 +0300

    created the exercises function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
 
+    if (num1 > num2){
+        [num1,num2] = [num2, num1];
+    }
+
+    let sum = 0;
+    for (;num1 <= num2; num1++) {
+        sum += num1;    
+    }
+    console.log(sum)
+    return sum;
 };
 
 // Do not edit below this line
