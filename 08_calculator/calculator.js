264cdaf4fc88814fe767845f4a26f0ca87c1b725
nikoroytio <roytioniko@gmail.com>
2023-09-28 15:01:09 +0300

    Did the exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,53 @@
 const add = function() {
-	
+  let result = arguments[0];
+  for (let i = 1; i < arguments.length; i++){
+    result += arguments[i];
+  }
+  return result
 };
 
 const subtract = function() {
-	
+  let result = arguments[0];
+  for (let i = 1; i < arguments.length; i++){
+    result -= arguments[i];	
+  }
+  return result
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let result = 0;
+  array.forEach(item => {
+    result +=item;
+  })
+  return result
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  
+  const result = array.reduce((total, currentValue) => total * currentValue, 1)
+  return result
 
 };
 
 const power = function() {
+  let result = arguments[0];
+  for (let i = 1; i < arguments.length; i++){
+    result **= arguments[i];	
+  }
+  return result
 	
 };
 
-const factorial = function() {
+const factorial = function(num) {
+  if (num < 0){
+    return -1;
+  }
+  else if (num == +0){
+    return 1;
+  }
+  else {
+    return (num * factorial(num-1))
+  }
 	
 };
 
