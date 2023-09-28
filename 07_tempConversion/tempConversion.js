be227a8b9eb17063a7d983721da71f2efc0b3890
nikoroytio <roytioniko@gmail.com>
2023-09-28 14:34:13 +0300

    Did the function for the exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  celsius = 5/9 * (temperature - 32)
+  return roundedCelsius = Math.round(celsius * 10) / 10
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  fahrenheit = (temperature * 9/5) + 32
+  return roundedFahrenheit = Math.round(fahrenheit * 10) / 10
 };
 
 // Do not edit below this line
