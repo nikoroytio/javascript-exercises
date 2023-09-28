08589c16e5df820659c248e0ec06da468a15a510
nikoroytio <roytioniko@gmail.com>
2023-09-28 15:13:13 +0300

    fixed regular expression from â to ^a

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,5 @@
 const palindromes = function (str) {
-    const cleanedString = str.toLowerCase().replace(/[â-z0-9]/g, "");
+    const cleanedString = str.toLowerCase().replace(/[^a-z0-9]/g, "");
     return cleanedString === cleanedString.split('').reverse().join('');
 
 };
