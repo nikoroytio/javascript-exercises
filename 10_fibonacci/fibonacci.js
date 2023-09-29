3b0686175a381b94be72a7dc9b8a91e88e94b94a
nikoroytio <roytioniko@gmail.com>
2023-09-29 08:36:56 +0300

    Did the exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n == 1 || n == 0) return parseInt(n);
 
+    else if (n < 0) return "OOPS"
+
+    // Initial values for the sequence
+    let fibSequence = [0, 1];
+
+    // Calculate and store each Fibonacci number up to n
+    for(let i = 2; i <= n; i++) {
+        fibSequence[i] = fibSequence[i-1] + fibSequence[i-2];
+    }
+
+    // Return the nth Fibonacci number
+    return parseInt(fibSequence[n]);
 };
 
 // Do not edit below this line
