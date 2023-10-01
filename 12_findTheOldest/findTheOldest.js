6ec9edf915ffc1e75972641ff283b012023b8259
nikoroytio <roytioniko@gmail.com>
2023-10-01 15:02:31 +0300

    Did the exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    if (!people || people.length === 0) {
+        return {};
+    }
+    
+    const currentYear = new Date().getFullYear();
+
+    const oldest = people.reduce((oldest, current) => {
+        const oldestDeathYear = oldest.yearOfDeath || currentYear;
+        const currentDeathYear = current.yearOfDeath || currentYear;
+
+        const oldestAge = oldestDeathYear - oldest.yearOfBirth;
+        const currentAge = currentDeathYear - current.yearOfBirth;
+
+        return currentAge > oldestAge ? current : oldest;
+        
+    });
+    
+    return oldest;
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
